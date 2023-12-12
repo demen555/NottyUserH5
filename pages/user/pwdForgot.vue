d33a29f4424376bdfdc3037fd033b1b1a45b77e8
Demen <Demen@example.com>
2023-12-12 09:54:04 +0800

    忘记密码邮件地址，点进去，输入好账号密码，submit 无效，没有请求

--- pages/user/pwdForgot.vue
+++ pages/user/pwdForgot.vue
@@ -112,7 +112,7 @@ export default {
             }
 
             this.showLoading = true;
-            requestEmailVerify({
+            this.$userApi.requestEmailVerify({
               hash: getQueryString('hash'),
               password: password,
             }).then( res => {
