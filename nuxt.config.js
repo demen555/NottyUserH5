d95a3461cb407e810d2c2ccde62d9773b0743a65
Demen <Demen@example.com>
2023-12-21 17:28:31 +0800

    较老手机不支持min()

--- nuxt.config.js
+++ nuxt.config.js
@@ -175,11 +175,11 @@ export default {
             minPixelValue: 1, // 小于1px不转换
             mediaQuery: false, // 允许在媒体查询中转换px
           },
-          'postcss-mobile-forever': {
-            "viewportWidth": 750,
-            "appSelector": "#__nuxt",
-            "maxDisplayWidth": 600
-          }
+          // 'postcss-mobile-forever': {
+          //   "viewportWidth": 750,
+          //   "appSelector": "#__nuxt",
+          //   "maxDisplayWidth": 600
+          // }
         },
       },
     },
