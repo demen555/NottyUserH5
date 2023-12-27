21e855139123675b768de2453cfd6a990bfb94b4
justin <justin@2box.me>
2023-12-27 16:25:07 +0800

    fix: page

--- pages/index.vue
+++ pages/index.vue
@@ -1,102 +1,154 @@
 <template>
-    <div class="home">
-      <HeaderTop @refresh="onRefresh"  id="home-top"></HeaderTop>
-      
-      <div class="loading-box" v-if="spainnerLoading">
-        <cardLoad></cardLoad>
-      </div>
-      <van-pull-refresh v-model="refreshing" @refresh="onRefresh" class="paddingTop52">
-         <!-- <p>刷新次数: {{ count }}</p> -->
-        <van-list
-          v-model="loading"
-          :finished="finished"
-          :finished-text="$t('str_no_more')"
-          :immediate-check='false'
-          @load="onLoad"
-        >
-          <Cover v-for="item in dataList" :item="item" :key="item.vodId"></Cover>
-        </van-list>
-      </van-pull-refresh>
+  <div class="home">
+    <HeaderTop @refresh="onRefresh"  id="home-top"></HeaderTop>
+    <div class="loading-box" v-if="spainnerLoading">
+      <cardLoad></cardLoad>
     </div>
+    <!-- <van-pull-refresh v-model="refreshing" @refresh="onRefresh" class="paddingTop52">
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        :finished-text="$t('str_no_more')"
+        :immediate-check='false'
+        @load="onLoad"
+      >
+        <Cover v-for="item in dataList" :item="item" :key="item.vodId"></Cover>
+      </van-list>
+    </van-pull-refresh> -->
+    <div class="video-list paddingTop52" >
+      <Cover v-for="item in dataList" :item="item" :key="item.vodId"></Cover>
+      <div class="home-footer">
+        <van-pagination class="pagination" v-model="pageInfo.page" :total-items="pageInfo.total" :show-page-size="4" force-ellipses >
+          <template #prev-text>
+            Prev
+          </template>
+        </van-pagination>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import { mapGetters } from "vuex"
 import HeaderTop from '~/components/header/top.vue'
 import Cover from '~/components/cover'
 import cardLoad from "~/components/skeleton/cardLoad.vue"
 import CODES from "~/plugins/enums/codes"
 export default{
-  data() {
-    return {
-      spainnerLoading: true,// 全局loading层
-      loading: false, // 是否处于加载状态
-      finished: false, // 是否加载完成
-      refreshing: false, // 当前是否刷新重置信息
-      dataList: [],
-      pageInfo: {
-        page: 1,
-        size: 60
-      }
+data() {
+  return {
+    spainnerLoading: true,// 全局loading层
+    loading: false, // 是否处于加载状态
+    finished: false, // 是否加载完成
+    refreshing: false, // 当前是否刷新重置信息
+    dataList: [],
+    pageInfo: {
+      page: 1,
+      size: 20
+    },
+    footerList: [
+      { name: 'CSAM Policy', id: 'policy-csam'},
+      { name: 'Content Removal', id: 'policy-content'},
+      { name: 'DMCA', id: 'policy-terms'},
+      { name: 'NCC Policy', id: 'policy-ncc'},
+      { name: '2257', id: 'policy-2257'},
+      { name: 'EU DSA', id: 'policy-eu'},
+    ],
+    hostname: ''
+  }
+},
+computed: {
+  ...mapGetters(['theme']),
+  themeChecked(){
+    return this.theme === 'dark'
+  }
+},
+created(){
+  if(process.client){
+    this.hostname = window.location.hostname
+  }
+  this.getList('first')
+},
+components: {
+  HeaderTop,
+  Cover,
+  cardLoad
+},
+watch: {
+  ['pageInfo.page'](val){
+    console.log(val, 'watch')
+    this.pageInfo.page = val
+    this.getList();
+    this.handleScroll()
+    // this.onLoad()
+  }
+},
+// async asyncData({ $homeApi }) {
+//   const res = await $homeApi.requestvodpageHome({ page: 1, size: 20})
+//   const { data } = res
+//   return { dataList: data.data }
+// },
+methods: {
+  handleScroll() {
+    if(process.client){
+      const target = document.querySelector('#home-top')
+      target.scrollIntoView({ behavior: 'smooth',  block: "start" })
     }
   },
-  created(){
-    this.getList('first')
+  handleClickPage(name){
+    this.$router.push({
+      name
+    })
   },
-  components: {
-    HeaderTop,
-    Cover,
-    cardLoad
+  onLoad(){
+    this.pageInfo.page += 1
+    this.getList();
   },
-  async asyncData({ $homeApi }) {
-    const res = await $homeApi.requestvodpageHome({ page: 1, size: 60})
-    const { data } = res
-    return { dataList: data.data }
-  },
-  methods: {
-    onLoad(){
-      this.pageInfo.page += 1
-      this.getList();
-    },
-    async getList(isRefresh ){
-      try {
-        isRefresh  === 'first' && (this.spainnerLoading = true)
-        // this.loading = true
-        const res = await this.$homeApi.requestvodpageHome({ page: this.pageInfo.page, size: this.pageInfo.size})
-        const { code, data } = res
-        
-        if(code == CODES.SUCCESS && data){
-          if(isRefresh){
-            this.dataList = data.data
-            this.refreshing = false
-          } else {
-            this.dataList = [ ...this.dataList, ...data.data]
-            this.loading = false
-          }
-         console.log( code, data, this.dataList, 'data' )
-          if(data.data.length === 0){
-            this.finished = true
-          }
-        }
-      } catch (error) {
-        console.error(error)
-      } finally {
-        this.spainnerLoading = false
-        this.refreshing = false
-        this.loading = false
+  async getList(isRefresh ){
+    try {
+      this.spainnerLoading = true
+      isRefresh  === 'first' && (this.spainnerLoading = true)
+      // this.loading = true
+      const res = await this.$homeApi.requestvodpageHome({ page: this.pageInfo.page, size: this.pageInfo.size})
+      const { code, data } = res
+      if(code == CODES.SUCCESS && data){
+        this.pageInfo.total = data.meta.pagination.total
+        this.dataList = data.data
+        console.log( code, data, this.dataList, 'data' )
       }
-    },
-    onRefresh() {
-      this.pageInfo.page = 1
-      this.getList(true);
-    },
-  }
+    } catch (error) {
+      console.error(error)
+    } finally {
+      this.spainnerLoading = false
+    }
+  },
+  onRefresh() {
+    this.pageInfo.page = 1
+    this.getList(true);
+  },
+}
 }
 </script>
 <style lang="less">
 .van-pull-refresh{
-  overflow: visible;
+overflow: visible;
 }
 .color{
-    color: var(--text-color3);
-    font-size: 15px;
+  color: var(--text-color3);
+  font-size: 15px;
+}
+.home-footer{
+  padding: 0 12px;
+  color: var(--text-color2);
+  // position: fixed;
+  // bottom: 0;
+  // left: 0;
+  // right: 0;
+  // z-index: 9999;
+  background-color: var(--bg-color1);
+  // padding-bottom: 24px;
+}
+.pagination{
+  margin-top: 12px !important;
+  margin-bottom: 12px !important;
 }
 </style>
