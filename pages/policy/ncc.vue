698383ceda7ff5ab6dadcbb214fb577ca3a970cd
justin <justin@2box.me>
2024-01-04 17:00:24 +0800

    feat: footer

--- pages/policy/ncc.vue
+++ pages/policy/ncc.vue
@@ -1,111 +1,100 @@
 <template>
   <div class="tag">
     <HeaderTop @refresh="onRefresh"></HeaderTop>
-    <Nav title="NCC Policy" text></Nav>
+    <Nav :title="str_footer_nav5" text></Nav>
     <div class="police">
-      <div class="police-title">Non-Consensual Content Policy</div>
-      <div class="police-title-text">August 2022</div>
+      <div class="police-title">{{ $t('str_footer_ncc1') }}</div>
+      <div class="police-title-text">{{ $t('str_footer_ncc5') }}</div>
       <div class="police-text-content">
-        Consent, as a rule and value, is paramount to the security of our users and integrity of our platform.
-We require all uploaders to affirm they have obtained and maintain valid consent and proper release documentation for all persons featured in all content uploaded to the platform. In addition, content must adhere to our Terms of Service.
-Content that we identify as a violation of our Non-Consensual Content Policy will be removed, and the associated user’s account actioned accordingly.
+        {{ $t('str_footer_ncc3') }}
       </div>
-      <div class="police-title">What is considered a violation of our non-consensual content policy:</div>
-      <div class="police-text-content">The recording or distribution of intimate images (that is, photos and videos that are generally understood to depict explicit sexual activity or nudity or partial nudity) without someone’s consent is a violation of that person’s privacy and is a severe violation of our Terms of Service, and is strictly prohibited. Content featuring or promoting non-consensual acts, real or simulated, is also prohibited.</div>
-      <div class="police-title">Guidelines</div>
-      <div class="police-text-content">Do NOT use the platform to upload, post, or otherwise share explicit images or videos that were taken, appear to have been taken or that were shared without the consent of the people involved. Examples of the types of content that violate this policy include, but are not limited to*:</div>
-      <div class="police-text-content-next">● Videos or photos of any person, whether in a public or private setting, without that person’s valid written consent (such as hidden/spy camera content, or up-skirt content);</div>
-      <div class="police-text-content-next">● Sharing explicit sexual images of someone else without their valid written consent (this would include, namely: non-consensual distribution of intimate images, also commonly referred to as “revenge porn”);</div>
-      <div class="police-text-content-next">● Sharing non-sexually explicit images of someone else without their valid written consent;</div>
-      <div class="police-text-content-next">● “Doxing”, defined as exposing private and identifying information about a particular individual, including phone number, e-mail, address, or full name).</div>
-      <div class="police-title" style="margin: 10px 0;">To help protect the integrity of the platform, we also prohibit content, including fictional, simulated, or animated, that features or depicts:</div>
-      <div class="police-text-content-next">● Depictions of non-consensual sexual activity*, including forced sexual acts, rape, or sexual assault;</div>
-      <div class="police-text-content-next">● Persons appearing drugged, incapacitated, or intoxicated** involved in sexual acts;</div>
-      <div class="police-text-content-next">● Any “deepfakes” whatsoever (manipulating a person’s image, whether in picture or video, to deceive or mislead the viewer into believing that person is acting or speaking in the way presented, absent artistic merit).</div>
-      <div class="police-text-content">* See our section below on Sensitive Themes and Considerations for Consent.</div>
-      <div class="police-text-content">** Intoxicated (including pretending to be) means a state induced by alcohol or drugs, whether legal, illicit, or recreational as determined by Nottyhub in its sole discretion, such that a performer’s judgment appears to the reasonable observer as objectively impaired.</div>
-      <div class="police-title">Sensitive Themes and Considerations for Consent</div>
-      <div class="police-text-content">It is important to remember that pornography does not always represent sexual interactions and behaviors typical of everyday life, and often depicts diverse fantasies and role-play scenarios by consenting amateurs and professionals. In all cases, we expect content to adhere to Nottyhub’s Terms of Service.
-When reviewing content depicting the sensitive themes listed below, we take the following into consideration to determine whether it violates our Terms of Service. We recommend reviewing our detailed policy before uploading content involving these sensitive themes. Consent must be determinable by the reasonable observer from the material itself, which may be assessed either through clear verbal or visual cues.
-Note that we consider many factors to distinguish fantasy from reality. The following is a non-exhaustive list of what we consider sensitive themes. Accordingly, content engaging with the following themes is subject to removal, and appropriate account enforcement, where found to be in violation of our Terms of Service:</div>
-      <div class="police-text-content-next">● Spells, Mesmerizing, or Possession: We do not permit content where:</div>
-      <div class="police-text-content-next-next">● A performer is depicted as incapacitated, or otherwise completely unconscious because of possession, spells, or mesmerizing;</div>
-      <div class="police-text-content-next-next">● Performers display signs of distress or panic during or after the act; or</div>
-      <div class="police-text-content-next-next">● Realistic depictions of spellbound, mesmerized, or possessed performers engaged in or subjected to sexual acts.</div>
-      <div class="police-text-content-next">● Sleep: Where there is a component of sleep in the content, the person must wake up within a reasonable time from the start of any sexual act(s) and consent must be made clear by that person.</div>
-      <div class="police-text-content-next">● Bondage and Restraints: If a scene features the use of a gag, restraint(s), or other device(s) that would render a performer unable to clearly speak or move, consent to continued participation must be clearly determinable from the material itself, or where it cannot be, consent must be clearly determinable through other visual or verbal cues.</div>
-      <div class="police-text-content-next">● Sub-Dom Fantasies: Consent must be determinable from the material itself, through clear verbal or visual cues</div>
-      <div class="police-text-content">To be clear, our rules with respect to these sensitive themes are not a judgment on these forms of sexual expression but aim to make clear what is or is not acceptable on our platform, to maintain a safe community.</div>
-      <div class="police-title">Enforcement</div>
-      <div class="police-text-content">We are steadfast in our commitment to protecting the safety of our users. As a content sharing platform, Nottyhub relies on technology, our team of human moderators, and our wider community of users to help identify violative content. We also actively cooperate with law-enforcement investigations and promptly respond to valid legal requests received in order to assist in combating the dissemination of non-consensual recordings and distribution on the Nottyhub platform.</div>
-      <div class="police-title">24/7 Human Moderation Team</div>
-      <div class="police-text-content">Our team of moderators and support staff work 24 hours a day, 7 days a week in order to review all uploaded content for violations of our Terms of Service, address user concerns, and remove all non-consensual content that we identify or of which we are made aware.</div>
-      <div class="police-title">Community Oversight and Detection</div>
-      <div class="police-text-content">Detecting non-consensual images at the time of upload is complex. While we use technology to combat the distribution of non-consensual intimate images and videos, we also rely on people proactively reporting that type of content to us. As such, members of our community play an important role in helping to secure the integrity and safety of our platform.
-If you have first-hand knowledge of content on the platform that violates these guidelines, or is otherwise illegal and abusive, you may alert us by filling out and submitting our Content Removal Request Form as soon as possible so that we can take appropriate and swift action.
-If you are a registered user, if you come across content, users, or comments that you think may be harmful for the community, you may also flag them to us for review.</div>
-      <div class="police-title">Automated Tools</div>
-      <div class="police-text-content">Content that is identified and removed for violating our non-consensual content policy is digitally fingerprinted using both the MediaWise® service from Vobile® and Safeguard, our own proprietary digital fingerprinting software. Digitally fingerprinting unauthorized content serves as an added layer of protection against further distribution of the content on our platform.
-We encourage users who have content in which they are featured, and who wish to prevent the upload of such content to our platform, to consider proactively digitally fingerprinting it.
-If you are interested in having your content digitally fingerprinted by Vobile you may contact them directly via email support@vobileinc.com.
-Should you wish to remove content that you: appear in and did not consent to have made and/or shared; OR content that is illegal and/or otherwise violates our Terms of Service, please report it to us.</div>
-      <div class="police-title">Consequences for violating this policy</div>
-      <div class="police-text-content">In all cases where we identify or are made aware of content that violates this policy, our Trust & Safety and Moderation teams work swiftly to:</div>
-      <div class="police-text-content-next">● Review and remove infringing content;</div>
-      <div class="police-text-content-next">● Fingerprint the content in question to help block future uploads of the same content to our sites; AND</div>
-      <div class="police-text-content-next">● Suspend or permanently terminate the associated uploader's account, where appropriate.</div>
-      <div class="police-title">How can you help us?</div>
-      <div class="police-text-content">If you believe you have come across non-consensual content, or any other content that otherwise violates our Terms of Service, we strongly encourage you to immediately alert us by flagging the content for our review.
-If you are the victim or have first-hand knowledge that content violates our Non-Consensual Content policy, please report the content to us by completing and submitting our Content Removal Request Form. Please include all relevant URL links to the content in question and we will address your request confidentially and remove the content expeditiously.
-Anyone can report violations of this policy using our Content Removal Request Form, whether they have an account on our platform or not.
+      <div class="police-title">{{ $t('str_footer_ncc4') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc5') }}</div>
+      <div class="police-title">{{ $t('str_footer_ncc6') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc7') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc8') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc9') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc10') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc11') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc12') }}</div>
+      <div class="police-title" style="margin: 10px 0;">{{ $t('str_footer_ncc13') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc14') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc15') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc16') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc17') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc18') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc19') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc20') }}</div>
+      <div class="police-text-content-next-next">{{ $t('str_footer_ncc21') }}</div>
+      <div class="police-text-content-next-next">{{ $t('str_footer_ncc22') }}</div>
+      <div class="police-text-content-next-next">{{ $t('str_footer_ncc23') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc24') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc25') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc26') }}s</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc27') }}</div>
+      <div class="police-title">{{ $t('str_footer_ncc28') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc29') }}</div>
+      <div class="police-title">{{ $t('str_footer_ncc30') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc31') }}</div>
+      <div class="police-title">{{ $t('str_footer_ncc32') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc33') }}</div>
+      <div class="police-title">{{ $t('str_footer_ncc34') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc35') }}</div>
+      <div class="police-title">{{ $t('str_footer_ncc36') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc37') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc38') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc39') }}</div>
+      <div class="police-text-content-next">{{ $t('str_footer_ncc40') }}</div>
+      <div class="police-title">{{ $t('str_footer_ncc41') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc42') }}
       </div>
-      <div class="police-title">Additional Resources and Support</div>
-      <div class="police-text-content">For more information, support, and referrals, you may wish to:
-        <br>Call someone you trust to support you
-        <br>Call your local law enforcement
-        <br>Contact a crisis line or organization dedicated to supporting and informing those in need. Below are just a few organizations, organized alphabetically according to country or region:
+      <div class="police-title">{{ $t('str_footer_ncc43') }}</div>
+      <div class="police-text-content">{{ $t('str_footer_ncc44') }}
+        <br>{{ $t('str_footer_ncc45') }}
+        <br>{{ $t('str_footer_ncc46') }}
+        <br>{{ $t('str_footer_ncc47') }}
       </div>
-      <div class="police-title">AUSTRALIA:</div>
+      <div class="police-title">{{ $t('str_footer_ncc48') }}</div>
       <div class="police-text-content">
-        Office of the esafety Commissioner Purpose is to help safeguard all Australians from online harms and to promote safer, more positive online experiences.
+        {{ $t('str_footer_ncc49') }}
         <br>https://www.esafety.gov.au/about-us
-        <br>Report forms | eSafety Commissioner
+        <br>{{ $t('str_footer_ncc50') }}
       </div>
-      <div class="police-title">BRAZIL:</div>
+      <div class="police-title">{{ $t('str_footer_ncc51') }}</div>
       <div class="police-text-content">
-        SaferNet
-        <br>SaferNet is an NGO that runs the Safer Internet Centre in Brazil, which offers a web-based helpline to support victims of online violence.
+        {{ $t('str_footer_ncc52') }}
+        <br>{{ $t('str_footer_ncc53') }}
         <br>p://www.safernet.org.br/site/webline
       </div>
-      <div class="police-title">BELGIUM:</div>
+      <div class="police-title">{{ $t('str_footer_ncc54') }}</div>
       <div class="police-text-content">
-        Belgian Institute for the Equality of Women and Men
+        {{ $t('str_footer_ncc55') }}
         <br>https://igvm-iefh.belgium.be/fr/institut
         <br>1800 12 800
         <br>egalite.hommesfemmes@iefh.belgique.be
       </div>
-      <div class="police-title">CANADA:</div>
+      <div class="police-title">{{ $t('str_footer_ncc56') }}</div>
       <div class="police-text-content">
-        Young Women's Christian Association (YWCA)
-        <br>A multi-service organization supporting women and girls against violence and human rights violations.
+        {{ $t('str_footer_ncc57') }}
+        <br>{{ $t('str_footer_ncc58') }}
         <br>ywcanada.ca/en > Guide on Sexual Image Based Abuse
       </div>
-      <div class="police-title">DENMARK:</div>
+      <div class="police-title">{{ $t('str_footer_ncc59') }}</div>
       <div class="police-text-content">
-        Stop Harassment (StopChikane) StopChikane is a counseling service for all people over the age of 18 who are exposed to digital violence and digital infringement.
+        {{ $t('str_footer_ncc60') }}
         <br>https://stopchikane.nu/om-stopchikane/hvem-er-stopchikane/
         <br>28 83 40 70
         <br>kontakt@stopchikane.nu
       </div>
-      <div class="police-title">FRANCE:</div>
+      <div class="police-title">{{ $t('str_footer_ncc61') }}</div>
       <div class="police-text-content">
-        Right to the Image (Droit à l’image)
-        <br>A French government website explaining the legal rights an individual has to his or her image, as well as pathways to request the removal of an image.
+        {{ $t('str_footer_ncc62') }}
+        <br>{{ $t('str_footer_ncc63') }}
         <br>https://service-public.fr/particuliers/vosdroits/F32103
       </div>
-      <div class="police-title">GERMANY:</div>
+      <div class="police-title">{{ $t('str_footer_ncc64') }}</div>
       <div class="police-text-content">
-        Association Concerning Sexual Violence Against Women
+        {{ $t('str_footer_ncc65') }}
         <br>https://rainlily.org.hk/chi/about
         <br>safechat@rainlily.org.hk
       </div>
