ada356e9935391a538e06c80d1ae5836aa05ab6e
Demen <Demen@example.com>
2024-01-10 16:30:50 +0800

    播放器优化

--- pages/video/video.vue
+++ pages/video/video.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="myVideo" id="HubmyVideo">
+    <div
+        @touchstart="handleTouchStart"
+        @touchend="handleTouchEnd" 
+        class="myVideo" 
+        id="HubmyVideo">
         <div id="dplayer" ref="player" class="dplayer video-box"></div>
         <div class="fast-forward-animation" v-show="fastWforward">
            <span></span>
@@ -10,6 +14,25 @@
            <span></span>
            <span></span>
            <b>{{ seekTime }}</b>
+        </div>
+        <div class="dplayer-top-controller" v-show="controller">
+            <div class="controller-tips">Long press the screen to speed up playback</div>
+            <div class="controller-btn">
+                <div class="speed" @click.stop="speedStatus = !speedStatus">{{nowSpeed}}x</div>
+                <div class="full" @click.stop="fullScreenBtn"></div>
+                <div class="speed-check" v-show="speedStatus">
+                    <span 
+                        :class="['item', { 'active': nowSpeed == item }]"
+                        @click.stop="changeVideoSpeed(item)"
+                        v-for="item in speed" 
+                        :key="item">
+                        {{ item }}x
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="controller-notice" v-show="showNotice">
+            2X{{ $t('str_playing') }}
         </div>
     </div>
 </template>
@@ -44,6 +67,13 @@ export default {
             clickTime: null,
             clickTgs: null,
             startTime: 0,
+
+            controller:  true,
+            speed:[ 0.5, 0.75, 1, 1.25, 1.5, 2 ],
+            nowSpeed: 1,
+            speedStatus: false,
+
+            showNotice: false
         }
     },
     props:{
@@ -104,7 +134,7 @@ export default {
                     }
                 },
             });
-
+            
             // 监听 DPlayer 播放事件
             this.dp.on('play', () => {
                 console.log("video_watch_start", new Date())
@@ -124,8 +154,54 @@ export default {
                     'value': videoWatchTimeInSeconds
                 });
             });
-            
+
+            // 目标元素
+            const targetElement = document.getElementById('dplayer');
+
+            // 定义一个回调函数处理变化
+            const callback = (mutationsList, observer) => {
+                for (const mutation of mutationsList) {
+                    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                        const hasYourClass = targetElement.classList.contains('dplayer-hide-controller');
+                        console.log(`元素是否有 dplayer-hide-controller 类：${hasYourClass}`);
+                        this.controller = !hasYourClass
+                    }
+                }
+            };
+
+            // 创建 MutationObserver 实例并配置
+            this.observer = new MutationObserver(callback);
+            const observerConfig = { attributes: true, attributeFilter: ['class'] };
+
+            // 启动 MutationObserver
+            this.observer.observe(targetElement, observerConfig);
+
         },
+
+        fullScreenBtn(){
+            this.dp.fullScreen.request();
+        },
+
+        changeVideoSpeed(item){
+            this.speedStatus = false;
+            this.nowSpeed = item;
+            this.dp.speed(item)
+        },
+
+        handleTouchStart() {
+            this.pressTimer = setTimeout(() => {
+                this.dp.speed(2)
+                this.nowSpeed = 2
+                this.showNotice = true
+            }, 500); // Adjust the duration for your needs
+        },
+        handleTouchEnd() {
+            clearTimeout(this.pressTimer);
+            this.dp.speed(1);
+            this.nowSpeed = 1
+            this.showNotice = false
+        },
+
 
         handleClickDebounce: debounce(function () {
             this.dp.seek(this.dp.video.currentTime + this.seekTime); // 快进 10 秒
@@ -152,6 +228,11 @@ export default {
 
             this.dp.destroy();
         }
+
+        if( this.observer ){
+            this.observer.disconnect();
+        }
+         
     },
 }
 </script>
@@ -298,6 +379,9 @@ export default {
 
 
 /deep/ .dplayer-mobile{
+    .dplayer-menu{
+        display: none !important;
+    }
     .dplayer-mobile-play{
         background: url("~~/static/images/bfq_play.png");
         background-size: 100% 100%;
@@ -331,9 +415,10 @@ export default {
     .dplayer-controller{
         padding: 0;
         .dplayer-icons-right{
-           position: absolute;
-           top: 0;
-           right: 0;
+            display: none;
+        //    position: absolute;
+        //    top: 0;
+        //    right: 0;
         }
         .dplayer-icons-left{
             position: absolute;
@@ -370,7 +455,105 @@ export default {
     }
 }
 
+.dplayer-top-controller{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 45px;
+    padding: 10px 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .controller-tips{
+        flex: 1;
+        color: var(--text-color2,  rgba(255, 255, 255, 0.70));
+        font-family: PingFang SC;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        opacity: 0;
+    }
+    .controller-btn{
+        margin-left: 4px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        position: relative;
+        .speed{
+            display: inline-flex;
+            padding: 3px 3.5px;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            border-radius: 4px;
+            border: 1px solid #FFF;
+            font-size: 8px;
+            background: var(--bg-color3, rgba(255, 255, 255, 0.10));
+            font-size: 12px;
+            zoom: 0.75;
+        }
+        .full{
+            width: 16px;
+            height: 16px;
+            flex-shrink: 0;
+            margin-left: 12px;
+            background-image: url('~~/static/images/bfq_qp.svg');
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+        }
+        .speed-check{
+            position: absolute;
+            top: 20px; 
+            right: 0;
+            background-color: var(--bg-color4, rgba(0, 0, 0, 0.6));
+            display: flex;
+            flex-direction: column;
+            width: 48px;
+            border-radius: 4px;
+            padding: 2px;
+            .item{
+                width: 100%;
+                height: 21px;
+                text-align: center;
+                font-size: 12px;
+                line-height: 21px;             
+            }
+            .active{
+                border-radius: 4px;
+                background: var(--text-color4, rgba(255, 255, 255, 0.20));
+                color: var(--f-6-d-658, #F6D658);
+            }
+        }
+    }
+}
 
+
+.controller-notice{
+    position: absolute;
+    // width: 60px;
+    height: 20px;
+    top: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+    display: inline-flex;
+    padding: 4px 8px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 4px;
+    background: rgba(0, 0, 0, 0.60);
+
+    color: var(--text-color1, #FFFFFF);
+    text-align: center;
+    font-family: PingFang SC;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+
+}
 
 </style>
 
