ddc86d0cb39c95810b2886f9c85aebec15227a9f
Demen <Demen@example.com>
2024-02-22 16:13:31 +0800

    个人中心UI样式

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <HeaderTop @refresh="onRefresh"></HeaderTop>
-    <div class="user-pc">
-
-    </div>
-    <div class="user-mobile d-md-none">   
-        <NavNew :title="$t('str_user_center')"></NavNew>
-        <div class="user-info ">
-            <div class="user-content avatar">
+    <div class="user container-fluid">   
+        <NavNew class="d-md-none" :title="$t('str_user_center')"></NavNew>
+        <h1 class="d-none d-md-block user-title" > {{ $t('str_user_center') }} </h1>
+        <div class="user-info row">
+            <div class="user-content avatar d-md-none">
                 <span class="title">{{ $t('str_header') }}</span>
                 <van-uploader :before-read="beforeRead">
                     <div class="img-icon">
@@ -18,18 +16,32 @@
                 </van-uploader>
             </div>
 
-            <div class="user-content account"> 
+            <div class="user-avatar d-none d-md-flex ">
+                <van-uploader :before-read="beforeRead">
+                    <div class="img-icon">
+                        <img class="img" v-if="userinfo.userPortrait" :src="userinfo.userPortrait" alt="avatar">
+                        <img class="img" v-else src="~/static/images/home_top_mrtx_1.svg" alt="avatar">
+                        <i :class="themeChecked?'icon': 'icon-white'"></i>
+                    </div>
+                </van-uploader>
+                <div class="user-name">
+                    <span> {{ userinfo.userName }} </span>
+                    <span> {{ userinfo.userEmail }} </span>
+                </div>
+            </div>
+
+            <div class="user-content account col-sm-12 col-md-4 col-lg-4 col-xl-4"> 
                 <span class="title">{{ $t('str_user_account') }}</span>
                 <span class="words">{{ userinfo.userName }}</span>
             </div>
-            <div class="user-content time">
+            <div class="user-content time col-sm-12 col-md-4 col-lg-4 col-xl-4">
                 <span class="title">{{ $t('str_register_time') }} </span>
                 <span class="words">{{ formatTime(userinfo.userRegTime) }}</span>
-                
             </div>
 
-            <div class="user-content pwdChange" @click="goPages('user-pwdChange')">
+            <div class="user-content pwdChange col-sm-12 col-md-4 col-lg-4 col-xl-4" @click="goPages('user-pwdChange')">
                 <span class="title"> {{ $t('str_change_pwd') }} </span>
+                <span class="words"> ****** </span>
                 <i class="words" :class="themeChecked?'icon': 'icon-white'"></i>
             </div>
 
@@ -188,5 +200,59 @@ export default {
 }
 :deep(.van-nav-bar__left){
   font-size: 18px;
+}
+@media (min-width: 1024px) {
+    .user{
+        margin: 96px auto 0;
+        overflow: hidden;
+        .user-title{
+            margin: 24px 0;
+        }
+        .user-info{
+            width: 1000px;
+            height: 185px;
+            border-radius: 24px;
+            background: rgba(255, 255, 255, 0.06);
+            margin: 0 auto;
+            padding: 24px;
+        }
+        .user-content{
+            &.col-sm-4, &.col-md-4, &.col-lg-4, &.col-xl-4{
+                flex-direction: column;
+                align-items: inherit;
+                // justify-content: inherit;
+                .words{
+                    text-align: left;
+                }
+            }
+        }
+        .user-avatar{
+            width: 100%;
+            height: 56px;
+            display: flex;
+            align-items: center;
+            .img-icon{
+                width: 56px;
+                height: 56px;
+                padding-right: 0;
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+            .user-name{
+                display: flex;
+                flex-direction: column;
+                margin-left: 8px;
+                span{
+                    margin: 4px 0;
+                    font-size: 16px;
+                    &:nth-child(2){
+                        color: rgba(255, 255, 255, 0.7);
+                    }
+                }
+            }
+        }
+    }
 }
 </style>
