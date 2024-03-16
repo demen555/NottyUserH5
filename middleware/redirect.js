d7d5d96eaea985e7eb7f6ae2a11c7161fb157fa0
Demen <Demen@example.com>
2024-03-16 17:36:07 +0800

    301 跳转

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -713,8 +713,8 @@ export default async function ({ route, redirect, app, $videoApi }) {
   }
 
   // 如果路由不匹配任何页面，重定向到当前语言的首页
-  // if (route.matched.length == 0) {
-  //   redirect(301, `/`)
-  // }
+  if (route.matched.length == 0) {
+    redirect(301, `/`)
+  }
 
 }
